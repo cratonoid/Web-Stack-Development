c870f799483b2a559fe7b40f98a9c8c786d49306
Alvis Abreo <68607388+cratonoid@users.noreply.github.com>
2023-01-11 09:15:09 +0530

    Add files via upload

---
+++ Lab 10/main.js
@@ -0,0 +1,162 @@
+const express = require("express")
+const app = express();
+const port = 3004;
+
+const mysql=require("./connection").con
+//configuration
+app.set("view engine","hbs");
+app.set("views", "./view");
+app.use(express.static(__dirname+"/public"))
+//Routing
+app.get("/",(req,res)=>{
+    res.render("index")
+});
+app.get("/add", (req, res) => {
+    res.render("add")
+
+});
+app.get("/search", (req, res) => {
+    res.render("search")
+
+});
+app.get("/update", (req, res) => {
+    res.render("update")
+
+});
+
+app.get("/delete", (req, res) => {
+    res.render("delete")
+
+});
+
+
+app.get("/view", (req, res) => {
+    let qry = "select * from intern ";
+    mysql.query(qry, (err, results) => {
+        if (err) throw err
+        else {
+            res.render("view", { data: results });
+        }
+
+    });
+
+});
+
+
+app.get("/addintern", (req, res) => {
+    // fetching data from form
+    const { name, phone, email, gender } = req.query
+
+    // Sanitization XSS...
+    let qry = "select * from intern  where emailid=? or phoneno=?";
+    mysql.query(qry, [email, phone], (err, results) => {
+        if (err)
+            throw err
+        else {
+
+            if (results.length > 0) {
+                res.render("add", { checkmesg: true })
+            } else {
+
+                // insert query
+                let qry2 = "insert into intern(username,phoneno,emailid,gender) values(?,?,?,?)";
+                mysql.query(qry2, [name, phone, email, gender], (err, results) => {
+                    if (results.affectedRows > 0) {
+                        res.render("add", { mesg: true })
+                    }
+                })
+            }
+        }
+    })
+});
+
+
+app.get("/searchintern", (req, res) => {
+    // fetch data from the form
+
+
+    const { phone } = req.query;
+
+    let qry = "select * from intern where phoneno=?";
+    mysql.query(qry, [phone], (err, results) => {
+        if (err) throw err
+        else {
+            if (results.length > 0) {
+                res.render("search", { mesg1: true, mesg2: false })
+                res.send(results);
+            } else {
+
+                res.render("search", { mesg1: false, mesg2: true })
+
+            }
+
+        }
+    });
+})
+
+app.get("/updatesearch", (req, res) => {
+
+    const { phone } = req.query;
+
+    let qry = "select * from intern where phoneno=?";
+    mysql.query(qry, [phone], (err, results) => {
+        if (err) throw err
+        else {
+            if (results.length > 0) {
+                res.render("update", { mesg1: true, mesg2: false, data: results })
+            } else {
+
+                res.render("update", { mesg1: false, mesg2: true })
+
+            }
+
+        }
+    });
+})
+app.get("/updateintern", (req, res) => {
+    // fetch data
+
+    const { phone, name, gender } = req.query;
+    let qry = "update intern set username=?, gender=? where phoneno=?";
+
+    mysql.query(qry, [name, gender, phone], (err, results) => {
+        if (err) throw err
+        else {
+            if (results.affectedRows > 0) {
+                res.render("update", { umesg: true })
+            }
+        }
+    })
+
+});
+
+app.get("/removeintern", (req, res) => {
+
+    // fetch data from the form
+
+
+    const { phone } = req.query;
+
+    let qry = "delete from intern where phoneno=?";
+    mysql.query(qry, [phone], (err, results) => {
+        if (err) throw err
+        else {
+            if (results.affectedRows > 0) {
+                res.render("delete", { mesg1: true, mesg2: false })
+            } else {
+
+                res.render("delete", { mesg1: false, mesg2: true })
+
+            }
+
+        }
+    });
+});
+
+
+app.listen(port,(err)=>{
+    if(err)
+        throw err
+    else
+        console.group("Server running at %d port",port);
+});
